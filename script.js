1afa8558b425f4fdb3c3ff3d7769f03c48eadcd3
erlend <ekrogstad@gmail.com>
2021-09-15 10:58:01 +0200

    added board rendering logic and player creation

--- script.js
+++ script.js
@@ -2,106 +2,76 @@
 
 const gameBoard = (() => {
     let container = document.querySelector("#container")
-    let gameBoardArray = [[[],[],[]],[[],[],[]],[[],[],[]]] //[[["x"],["x"],["x"]],[["o"],["x"],["o"]], [["o"],["o"],["o"]]]
-    let updateSquare = function ( squareId, player) {
-        let index = 0
-        this.gameBoardArray.forEach((row, i) => {
-            row.forEach((square, j) => {
-                if(squareId == index) {
-                    console.log({player})
-                    this.gameBoardArray[i][j] = player == 1 ? "X" : "O"  // whatever based on what palyer has: player.playerMark
-                    document.querySelector(`div[data-id="${index}"]`).innerText = this.gameBoardArray[i][j]
-
-                }
-                index++
-            })
-        })
-
-    }
-    let drawGameBoard = function () {
-        let squareId = 0;
-        let body = document.querySelector("body")
-        
-       
-        
-        this.gameBoardArray.forEach((element) => {
-            let rowDiv = document.createElement("div")
-            rowDiv.classList.add("row")
-            container.appendChild(rowDiv)
-            element.forEach((element) => {
+    let board;
+    let gameBoardArray = [["x","",""],["","o","x"],["","o",""]] //[[["x"],["x"],["x"]],[["o"],["x"],["o"]], [["o"],["o"],["o"]]]
+    let renderBoard = function() {
+        let squareDataId = 0
+        if(board) board.remove()
+        board = document.createElement("div")
+        board.id = "board"
+        container.appendChild(board)
+        gameBoardArray.forEach((rowEl, i) => {
+            let row = document.createElement("div")
+            row.classList.add("row")
+            board.appendChild(row)
+            rowEl.forEach((squareEl, j) => {
                 let square = document.createElement("div")
                 square.classList.add("square")
-                square.setAttribute("data-id", squareId)
-                square.innerText = element.length  ? element[0] : ""
-                square.addEventListener("click" , (e) =>{
-                    gameBoard.updateSquare(e.target.attributes["data-id"].value, game.playersTurn)
-                    console.log("click", game.playersTurn)
-                    game.toggleCurrentPlayer()
-                    console.log("after click",  game.playersTurn)
+                row.appendChild(square)
+                square.setAttribute("data-y", i)
+                square.setAttribute("data-x", j)
+                squareDataId++
+                //console.log(gameBoardArray[i][j])
+                square.innerText = gameBoardArray[i][j]
+                square.addEventListener("click", (e) => {
+                    console.log(e.target)
+                    let x = e.target.attributes["data-x"].value
+                    let y = e.target.attributes["data-y"].value
+                    console.log(x)
+                    makeMark(x, y)
+                    renderBoard()
                 })
-                rowDiv.appendChild(square)
-                squareId++;
             })
-        });
+        })
     }
-    
-    let clear = function () {
-        this.gameBoardArray = [[[],[],[]],[[],[],[]],[[],[],[]]] //clear the aray
-        container.querySelectorAll(".row").forEach(row => row.remove())
-        this.drawGameBoard()
+    let makeMark = function(x, y) {
+        if (gameBoardArray[y][x] === "") {
+            console.log("match")
+            gameBoardArray[y][x] = "x"
+        }
     }
-
+    let clear = function() {
+        gameBoardArray = [["","",""],["","",""],["","",""]]
+        renderBoard()
+    }
     return {
-        gameBoardArray,
-        drawGameBoard,
-        updateSquare,
-        clear
+        renderBoard,
+        clear,
+        
+        
     }
     
 })()
-//gameBoard.drawGameBoard()
+gameBoard.renderBoard()
 
 const displayController = (() => {
 
 })
 
-const player = (playernumber) => {
-    let playerNumber = playernumber
-    let playerScore = 0
-    let isPlayerTurn = false;
-    let playerMark = playerNumber == 1 ? "X" : "O" //X or O
-
+const player = (playernumber, playername = "Player " + playernumber) => {
+    let playerNumber = playernumber;
+    let mark = playerNumber - 1 ? "O" : "X" 
+    let playerName = playername
     return {
         playerNumber,
-        playerScore,
-        isPlayerTurn,
-        playerMark
+        mark,
+        playerName
     }
 }
 
 const game = (() => {
-    this.playersTurn = 1; //which players turn is it, 1 or 2. 
-    let toggleCurrentPlayer = function() {
-        this.playersTurn =  this.playersTurn - 1 ? 1 : 2
-        console.log("fwefew",playersTurn)
-    }
-    let startGame = function(){
-        gameBoard.drawGameBoard()
-        let playerOne = player(1)
-        let playerTwo = player(2)
-        console.log(playerOne)
-
-
-
-    }
-    return {
-        playersTurn,
-        toggleCurrentPlayer,
-        startGame,
-        self
-    }
+    
 
 
 })()
 
-game.startGame()
